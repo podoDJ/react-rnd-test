689bd9e2076ec8127b96f6a34f5ffe1fa916a3e0
podoDJ <ourstar1345@gmail.com>
2023-08-21 09:09:13 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { Rnd } from 'react-rnd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ImageBox = ({ imageUrl }) => (
+  <Rnd
+    default={{
+      x: Math.random() * 600, // Random initial x position
+      y: Math.random() * 200, // Random initial y position
+      width: "150px",
+      height: "150px",
+      
+    }}
+    minWidth={150}
+    minHeight={150}
+    // bounds="parent"
+    bounds="window"
+  >
+    <img src={imageUrl} draggable="false" alt="github avatar" width="150px" />
+  </Rnd>
+);
+
+const App = () => (
+  <div
+    style={{
+      width: '800px',
+      height: '400px',
+      border: '1px solid black',
+      margin: 'auto',
+      display: 'block'
+    }}
+  >
+    <ImageBox imageUrl="/rabbit.png" />
+    <ImageBox imageUrl="/shark.png" />
+  </div>
+);
 
 export default App;
